8d59bc5675cf9443e09b825ba2065702f6fcca14
Nikki Snow <hello@nikkisnow.com>
2013-06-05 05:37:03 -0400

    Command line arguments changed.

--- gittasks.py
+++ gittasks.py
@@ -1,20 +1,41 @@
 #!/usr/bin/env python
 
-import os, re, datetime, sys, hashlib, json, argparse
-from datetime import datetime
+import os
+import sys
+import re
+import hashlib
+import json
+import argparse
 import pytz
+import datetime
+from datetime import datetime
+
 
 class gitTasks(object):
 
     def __init__(self, options):
-        self.identifier = options.identifier
-        self.verbose = options.verbose
-        self.exclude = options.exclude.split(',')
+        if options.get('identifier'):
+            self.identifier = options['identifier']
+        else:
+            self.identifier = '@gt'
+        if options.get('verbose'):
+            self.verbose = options['verbose']
+        else:
+            self.verbose = False
+        if options.get('exclude'):
+            self.exclude = options['exclude'].split(',')
+        else:
+            self.exclude = [
+                'mp3', 'mp4', 'swf', 'git', 'png', 'gif', 'jpeg', 'jpg'
+            ]
+        self.exclude.append('gittasks')
+
         self.tasks = []
         self.tasksInFile = []
         self.tasksInCommit = []
         self.curDir = os.getcwd()
         self.firstRun = False
+        self.boldFormat = False
 
     def getCommitHash(self):
         log = os.popen("git log -1 HEAD")
@@ -24,7 +45,7 @@ class gitTasks(object):
             if (commitMatch):
                 commitLine = commitMatch.group()
                 c = commitLine.replace('commit ', '')
-                commit= c.strip()
+                commit = c.strip()
                 return commit
 
     def getAuthor(self):
@@ -60,11 +81,13 @@ class gitTasks(object):
             diff = self.getDiff()
             tasksInCommit = self.parse(diff)
             tasks = self.parseChanges(tasksInFile, tasksInCommit)
+            self.tasks = tasks
             self.saveTasks(tasks)
             self.feedback()
         else:
             self.firstRun = True
             tasks = self.parseRepository()
+            self.tasks = tasks
             self.saveTasks(tasks)
             self.feedback()
 
@@ -87,7 +110,7 @@ class gitTasks(object):
             return self.parseRepository()
 
     # Parse data
-    def parse(self, data = False):
+    def parse(self, data=False):
         f = ''
         thisTasks = []
         lineNumber = 0
@@ -121,7 +144,8 @@ class gitTasks(object):
                 if (gtMatch):
                     tasks = {}
                     initMatch = gtMatch.group()
-                    gtLine = re.search(re.escape(self.identifier) + "(.*)", initMatch)
+                    ident = re.escape(self.identifier)
+                    gtLine = re.search(ident + "(.*)", initMatch)
                     gtLine = gtLine.group()
                     gtLine = gtLine.replace(self.identifier, '')
                     gtLine = gtLine.strip()
@@ -144,7 +168,7 @@ class gitTasks(object):
                     if os.path.exists(self.curDir + '/' + f):
                         with open(self.curDir + '/' + f) as myFile:
                             for numA, lineA in enumerate(myFile, 1):
-                               if gtLine in lineA:
+                                if gtLine in lineA:
                                     lineNumber = numA
                     else:
                         # Probably means this task is completed
@@ -157,7 +181,8 @@ class gitTasks(object):
                     filePath = self.curDir + '/' + f
                     tasks['filePath'] = filePath
 
-                    # Hash the task and line number together for some semblance of uniqueness
+                    # Hash the task and line number together for some
+                    # semblance of uniqueness
                     h = hashlib.md5()
                     h.update(filePath + gtLine)
                     tasks['taskHash'] = h.hexdigest()
@@ -202,7 +227,8 @@ class gitTasks(object):
                             tasks['filePath'] = filePath
                             tasks['completed'] = ''
 
-                            # Hash the task and line number together for some semblance of uniqueness
+                            # Hash the task and line number together for some
+                            # semblance of uniqueness
                             h = hashlib.md5()
                             h.update(filePath + gtLine)
                             tasks['taskHash'] = h.hexdigest()
@@ -211,17 +237,19 @@ class gitTasks(object):
 
     def parseChanges(self, a, b):
         entries = []
-        for x, y in [(x,y) for x in a for y in b]:
+        for x, y in [(x, y) for x in a for y in b]:
 
-            # If the taskHash from the tasks in this commit isn't in the entries list, add it
+            # If the taskHash from the tasks in this commit isn't in the
+            # entries list, add it
             if not any(e['taskHash'] == y['taskHash'] for e in entries):
                 entries.append(y)
 
-            # If the taskHash from the tasks in file isn't in the entries list, add it
+            # If the taskHash from the tasks in file isn't in the entries list,
+            # add it
             if not any(e['taskHash'] == x['taskHash'] for e in entries):
                 entries.append(x)
 
-        for x, e in [(x,e) for x in self.tasksInCommit for e in entries]:
+        for x, e in [(x, e) for x in self.tasksInCommit for e in entries]:
             if e['taskHash'] == x['taskHash'] and x['operator'] == '-':
                 e['completed'] = self.getDate()
 
@@ -235,7 +263,12 @@ class gitTasks(object):
         else:
             # Else, open the file and save tasks
             target = open(self.curDir + '/.gittasks', 'w')
-            content = json.dumps(tasks, sort_keys=True, indent=4, separators=(',',':'))
+            content = json.dumps(
+                tasks,
+                sort_keys=True,
+                indent=4,
+                separators=(',', ':')
+            )
             target.write(content)
             target.close()
             return True
@@ -265,7 +298,7 @@ class gitTasks(object):
 
     def feedback(self):
         cnt = len(self.tasks)
-        if self.firstRun == True:
+        if self.firstRun is True:
             print str(cnt) + " gitTasks created"
         else:
             pass
@@ -274,12 +307,13 @@ class gitTasks(object):
         matches = []
         tasks = self.loadFile()
         print "Search for '%s'..." % term
+        print "'*' indicates task completion"
         for line in tasks:
             regex = r"(.*)" + str(term) + "(.*)"
             taskMatch = re.search(regex, line['task'], re.IGNORECASE)
             fileMatch = re.search(regex, line['filePath'], re.IGNORECASE)
             if taskMatch or fileMatch:
-                matches.append(self.formatTaskForDisplay(line))
+                matches.append(self.formatTaskForDisplay(line, True))
         if len(matches) > 0:
             if len(matches) == 1:
                 m = 'match'
@@ -290,7 +324,7 @@ class gitTasks(object):
         else:
             print "No matches found"
 
-    def formatTaskForDisplay(self, obj):
+    def formatTaskForDisplay(self, obj, is_search=False):
         task = []
         if self.verbose:
             task.append('> ' + obj['task'])
@@ -308,10 +342,28 @@ class gitTasks(object):
             if showLineNumber:
                 task.append('  Line Number: ' + str(obj['lineNumber']))
         else:
-            if obj['completed'] == '':
-                task.append("> %s, line %s, path: %s" % (obj['task'], str(obj['lineNumber']), obj['filePath']))
+            if obj['completed'] == '' or is_search:
+                dt = datetime.fromtimestamp(int(obj['date']))
+                d = dt.strftime('%Y-%m-%d %H:%M')
+                if obj['completed'] != '':
+                    completed = '* '
+                else:
+                    completed = ''
+                task.append("> %s%s %s, line %s, path: %s" % (
+                    completed,
+                    d,
+                    self.bold(obj['task']),
+                    str(obj['lineNumber']),
+                    obj['filePath'])
+                )
 
         return "\n".join(task)
+
+    def bold(self, msg):
+        if self.boldFormat:
+            return u'\033[1m%s\033[0m' % msg
+        else:
+            return msg
 
     def orderTasks(self, tasks, field, order):
         newTasks = sorted(tasks, key=lambda k: k[field])
@@ -319,7 +371,7 @@ class gitTasks(object):
             newTasks.reverse()
         return newTasks
 
-    def showTasks(self, showAll = False):
+    def showTasks(self, showAll=False):
         print "# gitTasks #"
         gitTasks = self.loadFile()
         gitTasks = self.orderTasks(gitTasks, 'date', 'asc')
@@ -328,49 +380,120 @@ class gitTasks(object):
             if task:
                 print task
 
+    def showHelp(self, command):
+        print command.upper() + " help"
+        if command == 'ls':
+            command = 'list'
+
+        help = {
+            'add': '''usage: gittasks.py add "My task item"
+
+Create a new task. The task is not tied to any file in your
+repository but the task will be related to the current repository.
+The current date is added as the task creation date. The task is
+assigned to you.
+            ''',
+            'search': '''usage: gittasks.py search term | gittasks.py search \
+"search string"
+
+Simple string case-insensitive matching on the repository's task list for the
+term. Multiple search terms require quotes and will match the entire string.
+            ''',
+            'list': '''usage: gittasks.py ls | gittasks.py list
+
+List all unfinished tasks. Optional parameters are as follows:
+
+  -v, --verbose        Show a more detailed task list
+
+            ''',
+            'verbose': '''usage: gittasks.py <command> -v | gittasks.py \
+<command> --verbose
+
+Display a more verbose task list.
+
+            '''
+        }
+        print help[command]
+
 # Create the parser
-parser = argparse.ArgumentParser(description="Manage task lists from a repository.")
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    prog="gittasks.py",
+    description='''
+Manage task lists from a repository.
 
-parser.add_argument("-i", "--identifier",
-    default="@gt",
-    help="Set the gitTasks identifier")
+Commands are:
+  ls, list             List all unfinished tasks
+  search               Simple text search performed on all tasks
+  add                  Create a new task, not tied to any file
+    ''',
+    epilog='''
+See 'gt help <command>' for more information on a specific command
+    ''')
 
-parser.add_argument("-s", "--search",
-    default=None,
-    metavar="TERM",
-    help="Simple text search performed on all tasks")
+parser.add_argument(
+    "cmd",
+    nargs="*",
+    help=argparse.SUPPRESS,
+)
 
-parser.add_argument("-v", "--verbose",
+parser.add_argument(
+    "-v", "--verbose",
     action="store_true",
-    dest="verbose",
     help="Run in verbose mode")
 
-parser.add_argument("-l", "--list",
-    action="store_true",
-    dest="list",
-    help="Display a task list; defaults to concise view")
+# parser.add_argument(
+#    "-a", "--all",
+#    action="store_true",
+#    help="Flag to display all tasks, regardless of completion")
 
-parser.add_argument("-e", "--exclude",
-    default="swf,pdf,psd,ai,png,gif,jpeg,jpg,mp3,aif,m4v,m4a", # Tons more but these will get you started
-    metavar='EXT',
+# parser.add_argument(
+#    "-f", "--format",
+#    action="store",
+#    help="Format the output task string")
+
+parser.add_argument(
+    "-e", "--exclude",
+    nargs="?",
+    action="store",
+    metavar="mp3,etc",
     help="Comma-delimited file extensions you wish to exclude")
 
-parser.add_argument("-c", "--create",
-    metavar='TASK',
-    help="Create a new task")
+parser.add_argument(
+    "-i", "--identifier",
+    nargs="?",
+    action="store",
+    metavar="@gt",
+    help="Set the gitTasks identifier. Default is @gt")
 
-options = parser.parse_args()
+opts = vars(parser.parse_args())
+command = opts.pop('cmd')
+options = {k: opts[k] for k in opts if opts[k] is not None}
 
 # Initialize the class:
 gitTasks = gitTasks(options)
 
 # Run the script ;)
-if options.search:
-    gitTasks.search(options.search)
-elif options.create:
-    gitTasks.createTask(options.create)
-elif options.list:
-    gitTasks.showTasks(options.verbose)
+if len(command) > 0:
+    if command[0] == 'help':
+        if len(command) > 1:
+            gitTasks.showHelp(command[1])
+        elif options:
+            for item in options:
+                if options[item] is True:
+                    gitTasks.showHelp(item)
+                    sys.exit()
+        else:
+            print "You must provide the command you want information on"
+    elif command[0] == 'search':
+        if len(command) > 1:
+            gitTasks.search(command[1])
+        else:
+            print "You must provide a search term"
+            sys.exit()
+    elif command[0] == 'add':
+        gitTasks.createTask(command[1])
+    elif command[0] in ['ls', 'list']:
+        gitTasks.showTasks()
 else:
     gitTasks.run()
-
