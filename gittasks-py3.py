36d77473935c9239cc864c8f6c7b73c3c4c1eafd
Nikki Snow <hello@nikkisnow.com>
2013-07-09 06:08:29 -0400

    Move import statements to one line each.

--- gittasks-py3.py
+++ gittasks-py3.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
-import os, re, datetime, sys, hashlib, json, argparse
+import os
+import re
+import datetime
 from datetime import datetime
+import sys
+import hashlib
+import json
+import argparse
 import pytz
-
